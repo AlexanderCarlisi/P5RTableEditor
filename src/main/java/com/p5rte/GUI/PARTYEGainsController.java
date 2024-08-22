a837d0548cf5f0a381dc0be4e8dd56894c86ebfc
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-08-22 16:00:24 -0400

    Add Stat Gains Editor

--- src/main/java/com/p5rte/GUI/PARTYEGainsController.java
+++ src/main/java/com/p5rte/GUI/PARTYEGainsController.java
@@ -26,6 +26,8 @@ public class PARTYEGainsController {
     private HBox manualStatsContainer;
     @FXML
     private Button generateStatGainsButton;
+    @FXML
+    private Label oddsTotalLabel;
 
 
     private Stage stage;
@@ -58,14 +60,37 @@ public class PARTYEGainsController {
             manualStatsField.setText("0");
         }
 
-        // Set Initial Values
-        viewLevelTextField.setText("1");
-
         // Add Listeners
-        viewLevelTextField.textProperty().addListener((__, oldValue, newValue) -> {
-            if (getLevelFromField(newValue.strip()) == getLevelFromField(oldValue.strip())) return;
+        viewLevelTextField.textProperty().addListener((__, ___, ____) -> {
             updateViewStatsLabel();
         });
+
+        // Randomizer Fields
+        generateStatGainsButton.setOnAction(e -> {
+            randomizeStatGains();
+        });
+
+        // Manual Gain Fields
+        manualLevelField.textProperty().addListener((__, ___, ____) -> {
+            updateManualGains();
+        });
+        for (int i = 0; i < 5; i++) {
+            final int INDEX = i;
+            manualStatsFields[i].textProperty().addListener((__, oldValue, newValue) -> {
+                if (getStatFromField(newValue.strip()) == getStatFromField(oldValue.strip())) return;
+                setManualGain(INDEX);
+            });
+
+            statOddsFields[i].textProperty().addListener((__, oldValue, newValue) -> {
+                if (getOddFromField(newValue.strip()) == getOddFromField(oldValue.strip())) return;
+                int total = 0;
+                for (int j = 0; j < 5; j++) {
+                    total += getOddFromField(statOddsFields[j].getText());
+                }
+                oddsTotalLabel.setText("Total Odds: " + total);
+            });
+        }
+
     }
 
 
@@ -74,7 +99,12 @@ public class PARTYEGainsController {
 
         instance.currentPersona = persona;
 
+        instance.viewLevelTextField.setText("1");
+        instance.statsPerLevelField.setText("3");
+        instance.manualLevelField.setText("1");
+
         updateViewStatsLabel();
+        updateManualGains();
 
         // for (int i = 0; i < 5; i++) {
         //     instance.statOddsFields[i].setDisable(disable);
@@ -86,62 +116,74 @@ public class PARTYEGainsController {
     }
 
 
-    // private static void randomizeStatGains() {
-        // if (instance == null) return;
+    private static void randomizeStatGains() {
+        if (instance == null) return;
 
-        // int[] statOdds = new int[5];
-        // int total = 0;
-        // for (int i = 0; i < 5; i++) {
-        //     statOdds[i] = parseInt(instance.statOddsFields[i].getText(), 0, 100);
-        //     total += statOdds[i];
-        // }
+        int[] statOdds = new int[5];
+        int total = 0;
+        int numbers = 0;
+        for (int i = 0; i < 5; i++) {
+            statOdds[i] = getOddFromField(instance.statOddsFields[i].getText());
+            total += statOdds[i];
+            if (statOdds[i] != 0) numbers++;
+        }
 
-        // if (total < 100) {
-        //     GUIManager.DisplayWarning (
-        //         "Stat Gains Odds Warning", 
-        //         "Stat Odd Total doesn't add to 100", 
-        //         "Stat Odds must add up to 100. Please adjust the values, then try again."
-        //     );
-        //     return;
-        // }
+        if (total != 100) {
+            GUIManager.DisplayWarning (
+                "Stat Gains Odds Warning", 
+                "Stat Odd Total doesn't add to 100", 
+                "Stat Odds must add up to 100. Please adjust the values, then try again."
+            );
+            return;
+        }
 
-        // int statsPerLevel = parseInt(instance.statsPerLevelField.getText(), 1, 5);
+        int statsPerLevel = getStatsPerLevelField(instance.statsPerLevelField.getText());
 
-        // int[][] statGain = new int[98][5];
+        if (numbers < statsPerLevel) {
+            GUIManager.DisplayWarning (
+                "Stat Gains Odds Warning", 
+                "Not Enough Stats to Increase", 
+                "A Stat only gets increased once per roll, so if the amount of ZERO odds you have is greater than statsPerLevel, then you get stuck in an infinite loop, not a bug, please adjust the values."
+            );
+            return;
+        }
 
-        // // Randomize Stat Gains, 
-        // // stats per level dictates the amount of stats to increase. eg: 3, then 3 stats will increase by 1
-        // // the same stat cannot be rolled again for the same level
-        // // the odds of a stat rolling is dictated by statOdds
-        // for (int l = 0; l < 98; l++) {
-        //     boolean[] statRolled = new boolean[5];
-        //     int remainingStats = statsPerLevel;
+        int[][] statGain = new int[98][5];
+
+        // Randomize Stat Gains, 
+        // stats per level dictates the amount of stats to increase. eg: 3, then 3 stats will increase by 1
+        // the same stat cannot be rolled again for the same level
+        // the odds of a stat rolling is dictated by statOdds
+        for (int l = 0; l < 98; l++) {
+            boolean[] statRolled = new boolean[5];
+            int remainingStats = statsPerLevel;
         
-        //     while (remainingStats > 0) {
-        //         int statRoll = (int) (Math.random() * 100);
-        //         int statIndex = 0;
+            while (remainingStats > 0) {
+                int statRoll = (int) (Math.random() * 100);
+                int statIndex = 0;
         
-        //         // Find the stat to increase based on the odds
-        //         for (int j = 0; j < 5; j++) {
-        //             if (statRoll < statOdds[j]) {
-        //                 statIndex = j;
-        //                 break;
-        //             }
-        //             statRoll -= statOdds[j];
-        //         }
+                // Find the stat to increase based on the odds
+                for (int j = 0; j < 5; j++) {
+                    if (statRoll < statOdds[j]) {
+                        statIndex = j;
+                        break;
+                    }
+                    statRoll -= statOdds[j];
+                }
         
-        //         // Only increase the stat if it hasn't been rolled yet
-        //         if (!statRolled[statIndex]) {
-        //             statRolled[statIndex] = true;
-        //             statGain[l][statIndex]++;
-        //             remainingStats--;
-        //         }
-        //     }
-        // }
+                // Only increase the stat if it hasn't been rolled yet
+                if (!statRolled[statIndex]) {
+                    statRolled[statIndex] = true;
+                    statGain[l][statIndex]++;
+                    remainingStats--;
+                }
+            }
+        }
 
-        // instance.currentPersona.statGain = statGain;
-        // updateStatViewer();
-    // }
+        instance.currentPersona.statGain = statGain;
+        updateManualGains();
+        updateViewStatsLabel();
+    }
    
 
     /**
@@ -172,6 +214,17 @@ public class PARTYEGainsController {
         return (stat < 0 || stat > 99) ? 0 : stat;
     }
 
+    private static int getOddFromField(String newValue) {
+        int odd = parseInt(newValue, 0);
+        return (odd < 0 || odd > 100) ? 0 : odd;
+    }
+
+    private static int getStatsPerLevelField(String newValue) {
+        int statsPerLevel = parseInt(newValue, 1);
+        return (statsPerLevel < 1 || statsPerLevel > 5) ? 1 : statsPerLevel;
+    }
+
+
     private static void updateViewStatsLabel() {
         if (instance == null) return;
 
@@ -190,5 +243,28 @@ public class PARTYEGainsController {
         }
 
         instance.viewStatsLabel.setText(statsText.substring(0, statsText.length() - 2));
+    }
+
+
+    private static void setManualGain(int index) {
+        if (instance == null) return;
+
+        int level = getLevelFromField(instance.manualLevelField.getText());
+        instance.currentPersona.statGain[level - 1][index] = getStatFromField(instance.manualStatsFields[index].getText());
+
+        updateViewStatsLabel();
+    }
+    
+
+    private static void updateManualGains() {
+        if (instance == null) return;
+
+        int level = getLevelFromField(instance.manualLevelField.getText());
+
+        for (int i = 0; i < 5; i++) {
+            instance.manualStatsFields[i].setText(String.valueOf(instance.currentPersona.statGain[level - 1][i]));
+        }
+
+        updateViewStatsLabel();
     }
 }
