e03e65792d67e79a2529080c6b966e83dbe5bfe3
Alexander Carlisi <alexandercarlisi0@gmail.com>
2024-09-21 11:58:27 -0400

    read persona names from file

--- src/main/java/com/p5rte/App.java
+++ src/main/java/com/p5rte/App.java
@@ -1,6 +1,7 @@
 package com.p5rte;
 
 import com.p5rte.GUI.GUIManager;
+import com.p5rte.Utils.Constants;
 
 /**
  * JavaFX App
@@ -8,6 +9,7 @@ import com.p5rte.GUI.GUIManager;
 public class App {
 
     public static void main(String[] args) {
+        Constants.init();
         GUIManager.launch(GUIManager.class, args);
     }
 }
